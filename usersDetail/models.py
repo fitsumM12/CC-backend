3c5b0d61364e594fe19b3928ececaa61b24d202a
Fitsum Mesfin <58865020+fitsumM12@users.noreply.github.com>
2025-07-15 22:16:28 +0300

    ✅✅✅✅✅✅

--- usersDetail/models.py
+++ usersDetail/models.py
@@ -3,8 +3,9 @@ from django.contrib.auth.models import AbstractUser, UserManager
 
 class HealthyInstitution(models.Model):
     id = models.AutoField(primary_key=True, auto_created=True)
-    name = models.TextField(max_length=200)
+    name = models.CharField(max_length=200, blank=True, null=True)
     created = models.DateTimeField(auto_now_add=True)
+    address = models.CharField(max_length=2000, null=True, blank=True)
     def __str__(self):
         return self.name
 
@@ -33,7 +34,7 @@ class usersDetail(AbstractUser):
     zone = models.CharField(max_length=100, blank=True, null=True)
     kebele = models.CharField(max_length=100, blank=True, null=True)
     hospital = models.CharField(max_length=100, blank=True, null=True)
-    health_institution = models.ForeignKey(HealthyInstitution, on_delete=models.CASCADE)
+    health_institution = models.ForeignKey(HealthyInstitution, on_delete=models.CASCADE, null=True, blank=True)
     image = models.CharField(max_length=100, blank=True, null=True)
     passport = models.CharField(max_length=100, blank=True, null=True)
     status = models.CharField(max_length=12, blank=True, null=True, choices=STATUS_CHOICES)
